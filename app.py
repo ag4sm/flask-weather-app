3727e0afa3d681c9f4ab7b3316f33fb1d4814adb
ag4sm <56977525+ag4sm@users.noreply.github.com>
2023-10-14 19:34:52 -0400

    Setup of Postgresql Database and updates to app

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
+import os
 import requests
 from flask import Flask, render_template
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
+# Register database to app
 db = SQLAlchemy(app)
-SQLAlchemy_database_URI
+migrate = Migrate(app, db)
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), null=False)
+    name = db.Column(db.String(50), nullable=False)
     
-
 
 @app.route('/', methods=['GET','POST'])
 def index():
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=77815caa8e7c8be3bf2e907b35a9817f'
-    city = 'Atlanta'
+    cities = City.query.all()
 
-    r = requests.get(url.format(city)).json()
-    weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'feels like': r['main']['feels_like'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-        'humidity': r['main']['humidity'],
-        'sunrise': r['sys']['sunrise'],
-        'sunset': r['sys']['sunset']
-    }
+    weatherdata = []
 
-    return render_template('weather.html',weather=weather)
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+        weather = {
+            'city': city,
+            'temperature': r['main']['temp'],
+            'feels like': r['main']['feels_like'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+            'humidity': r['main']['humidity'],
+            'sunrise': r['sys']['sunrise'],
+            'sunset': r['sys']['sunset']
+        }
+
+        weatherdata.append(weather)
+
+    return render_template('weather.html',weather=weatherdata)
